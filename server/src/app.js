f5e532df2ef00f917ddab299f03b3cf9be74ae0a
mmalekabs <mhd_malek@outlook.sa>
2023-09-07 04:10:30 +0300

    building react app

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,22 @@
+const {join} = require("path");
 const express = require('express');
 const cors = require('cors');
 
 const planetsRouter = require('./routes/planets/planets.router')
+const morgan = require("morgan");
 
 const app = express();
 
 app.use(cors({
     origin:'http://localhost:3000',
 }));
+app.use(morgan('combined'));
 app.use(express.json());
+app.use(express.static(join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
 
 module.exports = app;
