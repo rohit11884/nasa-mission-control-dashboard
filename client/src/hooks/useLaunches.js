0ce57dbee96cf2bbf16cc94dd88e13ac041adc6a
mmalekabs <mhd_malek@outlook.sa>
2023-09-12 00:31:29 +0300

    updating request.js & useLaunches.js

--- client/src/hooks/useLaunches.js
+++ client/src/hooks/useLaunches.js
@@ -34,8 +34,7 @@ function useLaunches(onSuccessSound, onAbortSound, onFailureSound) {
       target,
     });
 
-    // TODO: Set success based on response.
-    const success = false;
+    const success = response.ok;
     if (success) {
       getLaunches();
       setTimeout(() => {
@@ -51,7 +50,7 @@ function useLaunches(onSuccessSound, onAbortSound, onFailureSound) {
     const response = await httpAbortLaunch(id);
 
     // TODO: Set success based on response.
-    const success = false;
+    const success = response.ok;
     if (success) {
       getLaunches();
       onAbortSound();
